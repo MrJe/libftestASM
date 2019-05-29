41a36da880bae1974644d919cf7c9d902d231ede
Jessy Michaud <jmichaud@student.42.fr>
2019-05-29 18:15:52 +0200

    fix testeur

--- src/utils.c
+++ src/utils.c
@@ -18,16 +18,20 @@ int	cmp_files(int fd1, int fd2)
 {
 	ssize_t	f1_ret = 0;
 	ssize_t	f2_ret = 0;
-	char	*f1_buff[BUFF_SIZE];
-	char	*f2_buff[BUFF_SIZE];
+	char	*f1_buff[BUFF_SIZE + 1];
+	char	*f2_buff[BUFF_SIZE + 1];
 
+	bzero(f1_buff, BUFF_SIZE + 1);
+	bzero(f2_buff, BUFF_SIZE + 1);
 	while ((f1_ret = read(fd1, f1_buff, BUFF_SIZE)) >= 0 &&
 		(f2_ret = read(fd2, f2_buff, BUFF_SIZE)) >= 0)
 	{
-		if (f1_ret != f2_ret || !strncmp((char *)f1_buff, (char *)f2_buff, BUFF_SIZE))
+		if (f1_ret != f2_ret || strncmp((char *)f1_buff, (char *)f2_buff, BUFF_SIZE))
 			return (FAILURE);
 		if (f1_ret == 0 && f2_ret == 0)
 			return (SUCCESS);
+		bzero(f1_buff, BUFF_SIZE + 1);
+		bzero(f2_buff, BUFF_SIZE + 1);
 	}
 	return (FAILURE);
 }
